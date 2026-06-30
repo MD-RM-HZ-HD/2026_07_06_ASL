e5dbf8b11f95fe835269b0fe0a0ff27d5e389f33
HZHD25 <6340523@gmail.com>
2026-07-01 00:33:45 +0300

    Add files via upload

---
+++ data/data.Presentation.js
@@ -0,0 +1,15 @@
+const Presentation_Data = [
+    { title: "الدليل الشرعي اللفظي", audio: "Audio/1.1.0.m4a", pdf: "PDF/1.1.0.pdf", info: "info/1.1.0.png" },
+    { title: "حجية الظهور", audio: "Audio/1.1.1.m4a", pdf: "PDF/1.1.1.pdf", info: "info/1.1.1.png" },
+    { title: "تطبيقات حجية الظهور", audio: "Audio/1.1.2.m4a", pdf: "PDF/1.1.2.pdf", info: "info/1.1.2.png" },
+    { title: "اثبات الصدور", audio: "Audio/1.1.3.m4a", pdf: "PDF/1.1.3.pdf", info: "info/1.1.3.png" },
+    { title: "الدليل الشرعي غير اللفظي", audio: "Audio/1.2.0.m4a", pdf: "PDF/1.2.0.pdf", info: "info/1.2.0.png" },
+    { title: "الدليل العقلي", audio: "Audio/2.0.m4a", pdf: "PDF/2.0.pdf", info: "info/2.0.png" },
+    { title: "العلاقات القائمة بين نفس الأحكام", audio: "Audio/2.1.m4a", pdf: "PDF/2.1.pdf", info: "info/2.1.png" },
+    { title: "العلاقات القائمة بين الحكم وموضوعه", audio: "Audio/2.2.m4a", pdf: "PDF/2.2.pdf", info: "info/2.2.png" },
+    { title: "العلاقات القائمة بين الحكم ومتعلّقه", audio: "Audio/2.3.m4a", pdf: "PDF/2.3.pdf", info: "info/2.3.png" },
+    { title: "العلاقات القائمة بين الحكم والمقدّمات", audio: "Audio/2.4.m4a", pdf: "PDF/2.4.pdf", info: "info/2.4.png" },
+    { title: "العلاقات القائمة في داخل الحكم الواحد", audio: "Audio/2.5.m4a", pdf: "PDF/2.5.pdf", info: "info/2.5.png" },
+    { title: "الأصول العمليّة - القاعدة الأساسية والثانوية", audio: "Audio/3.0.m4a", pdf: "PDF/3.0.pdf", info: "info/3.0.png" },
+    { title: "قاعدة منجّزية العلم الإجمالي", audio: "Audio/3.1.m4a", pdf: "PDF/3.1.pdf", info: "info/3.1.png" }
+];
